48e95f7cf88a7ec5b8a85dd72bfcb9c6892ca927
acaiibawl <hagiya0719@gmail.com>
2022-01-12 11:37:25 +0900

    ブックマーク作成時の外部サイトメタ情報取得処理を別クラスに切り出し

--- src/app/Bookmark/UseCase/CreateBookmarkUseCase.php
+++ src/app/Bookmark/UseCase/CreateBookmarkUseCase.php
@@ -2,9 +2,9 @@
 
 namespace App\Bookmark\UseCase;
 
+use App\Lib\LinkPreview\LinkPreview;
 use App\Models\Bookmark;
 use Dotenv\Exception\ValidationException;
-use Dusterio\LinkPreview\Client;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 
@@ -30,18 +30,17 @@ final class CreateBookmarkUseCase
   {
     // 下記のサービスでも同様のことが実現できる
         // @see https://www.linkpreview.net/
-        $previewClient = new Client($url);
         try {
-            $preview = $previewClient->getPreview('general')->toArray();
+            $preview = (new LinkPreview())->get($url);
 
             $model = new Bookmark();
             $model->url = $url;
             $model->category_id = $category;
             $model->user_id = Auth::id();
             $model->comment = $comment;
-            $model->page_title = $preview['title'];
-            $model->page_description = $preview['description'];
-            $model->page_thumbnail_url = $preview['cover'];
+            $model->page_title = $preview->title;
+            $model->page_description = $preview->description;
+            $model->page_thumbnail_url = $preview->cover;
             $model->save();
         } catch (Exception $e) {
             Log::error($e->getMessage());
